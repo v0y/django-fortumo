a4c0182d09d8d3fa84ba19720834cfad700d3788
Rafał Mirończyk <voy@lolwtf.pl>
2015-04-29 02:10:25 +0200

    add fortumo ip validation

--- fortumo/views.py
+++ fortumo/views.py
@@ -1,5 +1,13 @@
+from django.conf import settings
 from django.http import HttpResponse
+from django.http.response import HttpResponseForbidden
 
 
 def payment_processor(request):
+    if (
+            settings.FORTUMO_ENABLE_IP_VALIDATION and
+            not request.META['REMOTE_ADDR'] in settings.FORTUMO_IPS
+    ):
+        return HttpResponseForbidden('403')
+
     return HttpResponse('dummy')
