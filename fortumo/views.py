effe769e1a3274291adb03238ef800d31d3468f5
Rafał Mirończyk <voy@lolwtf.pl>
2015-04-29 02:21:37 +0200

    add creating message objects on process payment

--- fortumo/views.py
+++ fortumo/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.http import HttpResponse
 from django.http.response import HttpResponseForbidden
+from fortumo.models import Message
 
 
 def payment_processor(request):
@@ -9,5 +10,25 @@ def payment_processor(request):
             not request.META['REMOTE_ADDR'] in settings.FORTUMO_IPS
     ):
         return HttpResponseForbidden('403')
+
+    # TODO: check signature
+
+    Message.objects.create(
+        message=request.GET['message'],
+        sender=request.GET['sender'],
+        country=request.GET['country'],
+        price=request.GET['price'],
+        price_wo_vat=request.GET['price_wo_vat'],
+        currency=request.GET['currency'],
+        service_id=request.GET['service_id'],
+        message_id=request.GET['message_id'],
+        keyword=request.GET['keyword'],
+        shortcode=request.GET['shortcode'],
+        operator=request.GET['operator'],
+        billing_type=request.GET['billing_type'],
+        status=request.GET['status'],
+        test=request.GET['test'],
+        sig=request.GET['sig'],
+    )
 
     return HttpResponse('dummy')
