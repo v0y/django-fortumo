55d07726877019d6219f7f6d1fe0a117dd5404e5
Rafał Mirończyk <voy@lolwtf.pl>
2015-05-10 22:12:01 +0200

    add models to admin

--- fortumo/admin.py
+++ fortumo/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Message,
+    Payment,
+    Service,
+)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('keyword', 'message', 'sender', 'shortcode')
+admin.site.register(Message, MessageAdmin)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('service', 'message', 'pin', 'used')
+admin.site.register(Payment, PaymentAdmin)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'service_id', 'ips', 'validate_ip')
+admin.site.register(Service, ServiceAdmin)
