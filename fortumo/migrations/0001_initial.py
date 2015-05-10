482c02f3518486544c33e5e22464ddeabb8fd12e
Rafał Mirończyk <voy@lolwtf.pl>
2015-05-10 21:50:00 +0200

    merge migrations

--- fortumo/migrations/0001_initial.py
+++ fortumo/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import json_field.fields
 
 
 class Migration(migrations.Migration):
@@ -13,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Message',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
                 ('message', models.CharField(max_length=64)),
                 ('sender', models.CharField(db_index=True, max_length=64)),
                 ('country', models.CharField(max_length=2)),
@@ -30,5 +31,30 @@ class Migration(migrations.Migration):
                 ('test', models.CharField(max_length=16)),
                 ('sig', models.CharField(max_length=128)),
             ],
+        ),
+        migrations.CreateModel(
+            name='Payment',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('pin', models.CharField(max_length=16, unique=True)),
+                ('used', models.BooleanField(default=False)),
+                ('message', models.OneToOneField(to='fortumo.Message')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Service',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=64)),
+                ('secret', models.CharField(max_length=128)),
+                ('service_id', models.CharField(max_length=128)),
+                ('ips', json_field.fields.JSONField(default=[], help_text='Enter a valid JSON object')),
+                ('validate_ip', models.BooleanField(default=True)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='payment',
+            name='service',
+            field=models.ForeignKey(related_name='payments', to='fortumo.Service'),
         ),
     ]
